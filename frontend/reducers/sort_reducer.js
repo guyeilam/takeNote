a5f17a250a52c371f483799208ae471b679e69f8
Guy Eilam <guyeilam@gmail.com>
2019-02-20 14:54:17 -0500

    Updated notebooks

--- frontend/reducers/sort_reducer.js
+++ frontend/reducers/sort_reducer.js
@@ -1,11 +1,11 @@
 import merge from 'lodash/merge';
 import { SET_SORT } from '../actions/ui_actions';
 
-export default function sortReducer(state = null, action) {
+export default function sortReducer(state = 'updated_date_descending', action) {
   let newState;
   switch (action.type) {
     case SET_SORT:
-      newState = action.sortMethod || null;
+      newState = action.sortMethod || 'updated_date_descending';
       return newState;
     default:
       return state;
