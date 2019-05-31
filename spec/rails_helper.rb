b3b32511b618432ad30e8ba37829e51ddf9c6d70
Guy Eilam <guyeilam@gmail.com>
2019-05-31 14:06:08 -0400

    Add User model test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,15 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'shoulda/matchers'
+
 # Add additional requires below this line. Rails is not loaded until this point!
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
