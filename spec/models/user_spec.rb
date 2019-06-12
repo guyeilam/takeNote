7ded454cfe48e8e0501484e9848e2255c0662617
Guy Eilam <guyeilam@gmail.com>
2019-06-12 10:54:17 -0400

    Add user validation rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
-describe User do
-  it { should validate_presence_of(:email) }
+# describe User do
+#   it { should validate_presence_of(:email) }
+# end
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
+    it { should validate_uniqueness_of(:email) }
+  end
 end
