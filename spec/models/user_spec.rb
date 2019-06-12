e309885971f1a05f8d09090d73ca97edbfd2a105
Guy Eilam <guyeilam@gmail.com>
2019-06-12 10:55:56 -0400

    Add user associations rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,11 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:session_token) }
     it { should validate_uniqueness_of(:email) }
   end
+
+  describe "associations" do
+    it { should have_many(:notebooks) }
+    it { should have_many(:notes) }
+    it { should have_many(:tags) }
+    it { should have_many(:shared_notes).through(:shares) }
+  end
 end
