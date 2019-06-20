200a129d4f374cd4c833b153ead3c5e308cbd53e
Guy Eilam <guyeilam@gmail.com>
2019-06-20 16:30:18 -0400

    Remove unused code

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-
-// import { login, signup, logout } from './actions/session_actions';
-// import { fetchAllNotebooks, fetchSingleNotebook, createNotebook, deleteNotebook, updateNotebook } from './util/notebook_api_util';
 import { requestAllNotebooks, requestSingleNotebook } from './actions/notebook_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -22,21 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-
-  // BEGIN TESTING
-  // window.login = login;
-  // window.signup = signup;
-  // window.logout = logout;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.requestAllNotebooks = requestAllNotebooks;
-  window.requestSingleNotebook = requestSingleNotebook;
-  // window.fetchAllNotebooks = fetchAllNotebooks;
-  // window.fetchSingleNotebook = fetchSingleNotebook;
-  // window.deleteNotebook = deleteNotebook;
-  // window.createNotebook = createNotebook;
-  // window.updateNotebook = updateNotebook;
-  // END TESTING
 
   ReactDOM.render(<Root store={store} />, root);
 });
