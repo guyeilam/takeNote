21e3057e1d3b4f485ba1e12433b5739607368554
Guy Eilam <guyeilam@gmail.com>
2019-07-02 15:09:56 -0400

    Update react-router-dom to v5.0.1

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -7,7 +7,7 @@ const Auth = ({ component: Component, path, loggedIn, exact }) => (
     !loggedIn ? (
       <Component {...props} />
     ) : (
-        <Redirect to="client" />
+        <Redirect to="/client" />
       )
   )} />
 );
