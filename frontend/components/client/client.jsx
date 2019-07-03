0e88056c6f95e15bae5e5b6b1885b2c4d274d772
Guy Eilam <guyeilam@gmail.com>
2019-07-03 10:52:47 -0400

    Format with prettier

--- frontend/components/client/client.jsx
+++ frontend/components/client/client.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Modal from '../modal/modal';
-import LeftNavBar from '../left_nav_bar/left_nav_bar';
-import NotebooksIndexContainer from '../notebooks/notebooks_index_container';
-import ViewNotebookContainer from '../notebooks/view_notebook';
-import { Route, withRouter } from 'react-router-dom';
-import TagsList from '../tags/tags_index';
-import LeftNavModal from '../left_nav_bar/left_nav_modal';
+import React from "react";
+import Modal from "../modal/modal";
+import LeftNavBar from "../left_nav_bar/left_nav_bar";
+import NotebooksIndexContainer from "../notebooks/notebooks_index_container";
+import ViewNotebookContainer from "../notebooks/view_notebook";
+import { Route, withRouter } from "react-router-dom";
+import TagsList from "../tags/tags_index";
+import LeftNavModal from "../left_nav_bar/left_nav_modal";
 
 const Client = () => {
   return (
@@ -13,45 +13,23 @@ const Client = () => {
       <Modal />
       <LeftNavModal modalId={null} />
 
-      <section className='notebooks'>
-
-        <div className='left-navbar'>
+      <section className="notebooks">
+        <div className="left-navbar">
           <LeftNavBar />
         </div>
 
-        <div className='left-navbar-spacer'></div>
-        
-        <Route exact
-          path="/client"
-          component={NotebooksIndexContainer}
-        />
-        <Route exact
-          path="/notebooks/:notebookId"
-          component={ViewNotebookContainer}
-        />
-        <Route exact
-          path="/notes/all"
-          component={ViewNotebookContainer}
-        />
-        <Route exact
-          path="/tags/:tagId"
-          component={ViewNotebookContainer}
-        />
-        <Route exact
-          path="/tags"
-          component={TagsList}
-        />
-        <Route exact
-          path="/search"
-          component={ViewNotebookContainer}
-        />
-        <Route exact
-          path="/shared_notes"
-          component={ViewNotebookContainer}
-        />
+        <div className="left-navbar-spacer" />
+
+        <Route exact path="/client" component={NotebooksIndexContainer} />
+        <Route exact path="/notebooks/:notebookId" component={ViewNotebookContainer} />
+        <Route exact path="/notes/all" component={ViewNotebookContainer} />
+        <Route exact path="/tags/:tagId" component={ViewNotebookContainer} />
+        <Route exact path="/tags" component={TagsList} />
+        <Route exact path="/search" component={ViewNotebookContainer} />
+        <Route exact path="/shared_notes" component={ViewNotebookContainer} />
       </section>
     </>
   );
-}
+};
 
 export default withRouter(Client);
