44c03e33a9b4b248777935af7fdfcd1e8d904cfb
Guy Eilam <guyeilam@gmail.com>
2019-07-10 14:38:28 -0400

    Remove unused code and format with prettier

--- frontend/actions/tag_actions.js
+++ frontend/actions/tag_actions.js
@@ -1,146 +1,93 @@
-import * as TagAPIUtil from '../util/tag_api_util';
+import * as TagAPIUtil from "../util/tag_api_util";
 
-export const RECEIVE_ALL_TAGS = 'RECEIVE_ALL_TAGS';
-export const RECEIVE_SINGLE_TAG = 'RECEIVE_SINGLE_TAG';
-export const RECEIVE_NEW_TAG = 'RECEIVE_NEW_TAG';
-export const RECEIVE_UPDATED_TAG = 'RECEIVE_UPDATED_TAG';
-export const RECEIVE_UPDATED_TAGGING = 'RECEIVE_UPDATED_TAGGING';
-export const REMOVE_TAG = 'REMOVE_TAG';
-export const RECEIVE_TAG_ERRORS = 'RECEIVE_TAG_ERRORS';
+export const RECEIVE_ALL_TAGS = "RECEIVE_ALL_TAGS";
+export const RECEIVE_SINGLE_TAG = "RECEIVE_SINGLE_TAG";
+export const RECEIVE_NEW_TAG = "RECEIVE_NEW_TAG";
+export const RECEIVE_UPDATED_TAG = "RECEIVE_UPDATED_TAG";
+export const RECEIVE_UPDATED_TAGGING = "RECEIVE_UPDATED_TAGGING";
+export const REMOVE_TAG = "REMOVE_TAG";
+export const RECEIVE_TAG_ERRORS = "RECEIVE_TAG_ERRORS";
 
-export const receiveAllTags = (payload) => {
-  return ({
-    type: RECEIVE_ALL_TAGS,
-    payload
-  })
-}
+export const receiveAllTags = payload => ({
+  type: RECEIVE_ALL_TAGS,
+  payload
+});
 
-export const receiveNewTag = (payload) => {
-  return ({
-    type: RECEIVE_NEW_TAG,
-    payload
-  })
-}
+export const receiveNewTag = payload => ({
+  type: RECEIVE_NEW_TAG,
+  payload
+});
 
-export const requestAllTags = () => {
-  return (dispatch) => {
-    return TagAPIUtil.fetchAllTags().then((tags) => {
-      return dispatch(receiveAllTags(tags));
-    },
-      (err) => {
-        return dispatch(receiveTagErrors(err.responseJSON));
-    });
-  }
-}
+export const requestAllTags = () => dispatch => {
+  return TagAPIUtil.fetchAllTags().then(
+    tags => dispatch(receiveAllTags(tags)),
+    err => dispatch(receiveTagErrors(err.responseJSON))
+  );
+};
 
-export const receiveSingleTag = payload => {
-  return ({
-    type: RECEIVE_SINGLE_TAG,
-    payload
-  });
-}
+export const receiveSingleTag = payload => ({
+  type: RECEIVE_SINGLE_TAG,
+  payload
+});
 
-export const receiveUpdatedTag = payload => {
-  return ({
-    type: RECEIVE_UPDATED_TAG,
-    payload
-  });
-}
+export const receiveUpdatedTag = payload => ({
+  type: RECEIVE_UPDATED_TAG,
+  payload
+});
 
-export const receiveUpdatedTagging = payload => {
-  return ({
-    type: RECEIVE_UPDATED_TAGGING,
-    payload
-  });
-}
+export const receiveUpdatedTagging = payload => ({
+  type: RECEIVE_UPDATED_TAGGING,
+  payload
+});
 
-export const requestSingleTag = (tagId) => {
-  return (dispatch) => {
-    return TagAPIUtil.fetchSingleTag(tagId).then((payload) => {
-      return dispatch(receiveSingleTag(payload));
-    },
-      (err) => {
-        return dispatch(receiveTagErrors(err.responseJSON));
-      });
-  }
-}
+export const requestSingleTag = tagId => dispatch => {
+  return TagAPIUtil.fetchSingleTag(tagId).then(
+    payload => dispatch(receiveSingleTag(payload)),
+    err => dispatch(receiveTagErrors(err.responseJSON))
+  );
+};
 
-export const createTag = (tag) => {
-  return (dispatch) => {
-    return TagAPIUtil.createTag(tag).then((tag) => {
-      return dispatch(receiveNewTag(tag));
-    },
-      (err) => {
-        return dispatch(receiveTagErrors(err.responseJSON));
-      });
-  }
-}
+export const createTag = tag => dispatch => {
+  return TagAPIUtil.createTag(tag).then(
+    tag => dispatch(receiveNewTag(tag)),
+    err => dispatch(receiveTagErrors(err.responseJSON))
+  );
+};
 
-export const updateTag = (tag) => {
-  return (dispatch) => {
-    return TagAPIUtil.updateTag(tag).then((tag) => {
-      return dispatch(receiveUpdatedTag(tag));
-    },
-      (err) => {
-        return dispatch(receiveTagErrors(err.responseJSON));
-      });
-  }
-}
+export const updateTag = tag => dispatch => {
+  return TagAPIUtil.updateTag(tag).then(
+    tag => dispatch(receiveUpdatedTag(tag)),
+    err => dispatch(receiveTagErrors(err.responseJSON))
+  );
+};
 
-export const deleteTag = (tag) => {
-  return (dispatch) => {
-    return TagAPIUtil.deleteTag(tag.id).then(() => {
-      return dispatch(removeTag(tag));
-    },
-      (err) => {
-        return dispatch(receiveTagErrors(err.responseJSON));
-      });
-  }
-}
+export const deleteTag = tag => dispatch => {
+  return TagAPIUtil.deleteTag(tag.id).then(
+    () => dispatch(removeTag(tag)),
+    err => dispatch(receiveTagErrors(err.responseJSON))
+  );
+};
 
-export const receiveTagErrors = (errors) => {
-  return ({
-    type: RECEIVE_TAG_ERRORS,
-    errors
-  });
-}
+export const receiveTagErrors = errors => ({
+  type: RECEIVE_TAG_ERRORS,
+  errors
+});
 
-export const removeTag = (tag) => {
-  return ({
-    type: REMOVE_TAG,
-    tag
-  });
-}
+export const removeTag = tag => ({
+  type: REMOVE_TAG,
+  tag
+});
 
-// export const requestTaggings = (tagId) => {
-//   return (dispatch) => {
-//     return TagAPIUtil.fetchTaggings(tagId).then((tags) => {
-//       return dispatch(receiveAllTags(tags));
-//     },
-//       (err) => {
-//         return dispatch(receiveTagErrors(err.responseJSON));
-//       });
-//   }
-// }
+export const createTagging = (tagId, noteId) => dispatch => {
+  return TagAPIUtil.createTagging(tagId, noteId).then(
+    payload => dispatch(receiveUpdatedTagging(payload)),
+    err => dispatch(receiveTagErrors(err.responseJSON))
+  );
+};
 
-export const createTagging = (tagId, noteId) => {
-  return (dispatch) => {
-    return TagAPIUtil.createTagging(tagId, noteId).then((payload) => {
-      return dispatch(receiveUpdatedTagging(payload));
-    },
-      (err) => {
-        return dispatch(receiveTagErrors(err.responseJSON));
-      });
-  }
-}
-
-export const deleteTagging = (tagId, noteId) => {
-  return (dispatch) => {
-    return TagAPIUtil.deleteTagging(tagId, noteId).then((payload) => {
-      return dispatch(receiveUpdatedTagging(payload));
-    },
-      (err) => {
-        return dispatch(receiveTagErrors(err.responseJSON));
-      });
-  }
-}
+export const deleteTagging = (tagId, noteId) => dispatch => {
+  return TagAPIUtil.deleteTagging(tagId, noteId).then(
+    payload => dispatch(receiveUpdatedTagging(payload)),
+    err => dispatch(receiveTagErrors(err.responseJSON))
+  );
+};
--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,22 +1,9 @@
-import * as UserAPIUtil from '../util/user_api_util';
-import { receiveCurrentUser, receiveErrors } from './session_actions';
+import * as UserAPIUtil from "../util/user_api_util";
+import { receiveCurrentUser, receiveErrors } from "./session_actions";
 
-// export const UPDATE_DEFAULT_NOTEBOOK = 'UPDATE_DEFAULT_NOTEBOOK';
-
-// export const updateDefaultNotebook = (notebookId) => {
-//   return ({
-//     type: UPDATE_DEFAULT_NOTEBOOK,
-//     notebookId
-//   });
-// }
-
-export const updateDefaultNotebook = (notebookId) => {
-  return (dispatch) => {
-    return UserAPIUtil.updateDefaultNotebook(notebookId).then((current_user) => {
-      return dispatch(receiveCurrentUser(current_user));
-    },
-      (err) => {
-        return dispatch(receiveErrors(err.responseJSON));
-      });
-  }
-}
+export const updateDefaultNotebook = notebookId => dispatch => {
+  return UserAPIUtil.updateDefaultNotebook(notebookId).then(
+    current_user => dispatch(receiveCurrentUser(current_user)),
+    err => dispatch(receiveErrors(err.responseJSON))
+  );
+};
