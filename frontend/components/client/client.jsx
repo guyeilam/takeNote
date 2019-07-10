941b9886cbb2f85a21c683643f368364d79c6604
Guy Eilam <guyeilam@gmail.com>
2019-07-10 15:05:49 -0400

    Remove loading icon from client component

--- frontend/components/client/client.jsx
+++ frontend/components/client/client.jsx
@@ -1,23 +1,18 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import Modal from "../modal/modal";
 import LeftNavBar from "../left_nav_bar/left_nav_bar";
-// import NotebooksIndexContainer from "../notebooks/notebooks_index_container";
 import ViewNotebookContainer from "../notebooks/view_notebook";
 import { Route, withRouter } from "react-router-dom";
 import TagsList from "../tags/tags_index";
 import LeftNavModal from "../left_nav_bar/left_nav_modal";
 import NotebooksIndex from "../notebooks/notebooks_index";
-import LoadingIcon from "../loading_icon/loading_icon";
 
 const Client = () => {
-  const loading = useSelector(state => state.ui.loading);
-
   return (
     <>
       <Modal />
       <LeftNavModal modalId={null} />
-      { loading ? <LoadingIcon /> : null }
+
       <section className="notebooks">
         <div className="left-navbar">
           <LeftNavBar />
@@ -26,7 +21,11 @@ const Client = () => {
         <div className="left-navbar-spacer" />
 
         <Route exact path="/client" component={NotebooksIndex} />
-        <Route exact path="/notebooks/:notebookId" component={ViewNotebookContainer} />
+        <Route
+          exact
+          path="/notebooks/:notebookId"
+          component={ViewNotebookContainer}
+        />
         <Route exact path="/notes/all" component={ViewNotebookContainer} />
         <Route exact path="/tags/:tagId" component={ViewNotebookContainer} />
         <Route exact path="/tags" component={TagsList} />
