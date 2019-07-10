022a3911be5ce867c3b5ba4afb20eb69d018013b
Guy Eilam <guyeilam@gmail.com>
2019-07-10 13:50:01 -0400

    Clean up notebook actions and format with prettier

--- frontend/actions/notebook_actions.js
+++ frontend/actions/notebook_actions.js
@@ -1,143 +1,91 @@
-import * as NotebookAPIUtil from '../util/notebook_api_util';
+import * as NotebookAPIUtil from "../util/notebook_api_util";
 
-export const RECEIVE_ALL_NOTEBOOKS = 'RECEIVE_ALL_NOTEBOOKS';
-export const RECEIVE_SINGLE_NOTEBOOK = 'RECEIVE_SINGLE_NOTEBOOK';
-export const REMOVE_NOTEBOOK = 'REMOVE_NOTEBOOK';
-export const RECEIVE_NOTEBOOK_ERRORS = 'RECEIVE_NOTEBOOK_ERRORS';
-export const RECEIVE_UPDATED_NOTEBOOK = 'RECEIVE_UPDATED_NOTEBOOK';
-export const REMOVE_NOTE_FROM_NOTEBOOK = 'REMOVE_NOTE_FROM_NOTEBOOK';
-export const START_LOADING_NOTEBOOKS = 'START_LOADING_NOTEBOOKS';
+export const RECEIVE_ALL_NOTEBOOKS = "RECEIVE_ALL_NOTEBOOKS";
+export const RECEIVE_SINGLE_NOTEBOOK = "RECEIVE_SINGLE_NOTEBOOK";
+export const REMOVE_NOTEBOOK = "REMOVE_NOTEBOOK";
+export const RECEIVE_NOTEBOOK_ERRORS = "RECEIVE_NOTEBOOK_ERRORS";
+export const RECEIVE_UPDATED_NOTEBOOK = "RECEIVE_UPDATED_NOTEBOOK";
+export const REMOVE_NOTE_FROM_NOTEBOOK = "REMOVE_NOTE_FROM_NOTEBOOK";
+export const START_LOADING_NOTEBOOKS = "START_LOADING_NOTEBOOKS";
 
-export const loadNotebooks = () => {
-  return ({
-    type: START_LOADING_NOTEBOOKS
-  });
-}
+export const loadNotebooks = () => ({
+  type: START_LOADING_NOTEBOOKS
+});
 
-export const receiveAllNotebooks = (payload) => {
-  return ({
-    type: RECEIVE_ALL_NOTEBOOKS,
-    payload
-  });
-}
+export const receiveAllNotebooks = payload => ({
+  type: RECEIVE_ALL_NOTEBOOKS,
+  payload
+});
 
 export const requestAllNotebooks = () => dispatch => {
   dispatch(loadNotebooks());
-  return NotebookAPIUtil.fetchAllNotebooks().then(payload => (
-    dispatch(receiveAllNotebooks(payload))
-  ),
-  err => dispatch(receiveNotebookErrors(err.responseJSON))
-  )
-}
+  return NotebookAPIUtil.fetchAllNotebooks().then(
+    payload => dispatch(receiveAllNotebooks(payload)),
+    err => dispatch(receiveNotebookErrors(err.responseJSON))
+  );
+};
 
-// export const requestAllNotebooks = () => {
-//   return (dispatch) => {
-//     dispatch(loadNotebooks());
-//     return NotebookAPIUtil.fetchAllNotebooks().then( (payload) => {
-//       return dispatch(receiveAllNotebooks(payload));
-//     },
-//       (err) => {
-//         return dispatch(receiveNotebookErrors(err.responseJSON));
-//       });
-//   }
-// }
+export const receiveSingleNotebook = payload => ({
+  type: RECEIVE_SINGLE_NOTEBOOK,
+  payload
+});
 
-export const receiveSingleNotebook = (payload) => {
-  return ({
-    type: RECEIVE_SINGLE_NOTEBOOK,
-    payload
-  });
-}
+export const requestSingleNotebook = notebookId => dispatch => {
+  dispatch(loadNotebooks());
+  return NotebookAPIUtil.fetchSingleNotebook(notebookId).then(
+    payload => dispatch(receiveSingleNotebook(payload)),
+    err => dispatch(receiveNotebookErrors(err.responseJSON))
+  );
+};
 
+export const receiveUpdatedNotebook = payload => ({
+  type: RECEIVE_UPDATED_NOTEBOOK,
+  payload
+});
 
-export const requestSingleNotebook = (notebookId) => {
-  return (dispatch) => {
-    dispatch(loadNotebooks());
-    return NotebookAPIUtil.fetchSingleNotebook(notebookId).then((payload) => {
-        dispatch(receiveSingleNotebook(payload));
-    },
-      (err) => {
-        return dispatch(receiveNotebookErrors(err.responseJSON));
-      });
-  }
-}
+export const removeNotebook = notebookId => ({
+  type: REMOVE_NOTEBOOK,
+  notebookId
+});
 
+export const receiveNotebookErrors = errors => ({
+  type: RECEIVE_NOTEBOOK_ERRORS,
+  errors
+});
 
-export const receiveUpdatedNotebook = (payload) => {
-  return ({
-    type: RECEIVE_UPDATED_NOTEBOOK,
-    payload
-  });
-}
+export const removeNoteFromNotebook = noteId => ({
+  type: REMOVE_NOTE_FROM_NOTEBOOK,
+  noteId
+});
 
-export const removeNotebook = (notebookId) => {
-  return ({
-    type: REMOVE_NOTEBOOK,
-    notebookId
-  });
-}
+export const updateNotebook = notebook => dispatch => {
+  dispatch(loadNotebooks());
+  return NotebookAPIUtil.updateNotebook(notebook).then(
+    notebook => dispatch(receiveUpdatedNotebook(notebook)),
+    err => dispatch(receiveNotebookErrors(err.responseJSON))
+  );
+};
 
-export const receiveNotebookErrors = (errors) => {
-  return ({
-    type: RECEIVE_NOTEBOOK_ERRORS,
-    errors
-  });
-}
+export const createNotebook = notebook => dispatch => {
+  dispatch(loadNotebooks());
+  return NotebookAPIUtil.createNotebook(notebook).then(
+    notebook => dispatch(receiveUpdatedNotebook(notebook)),
+    err => dispatch(receiveNotebookErrors(err.responseJSON))
+  );
+};
 
-export const removeNoteFromNotebook = (noteId) => {
-  return ({
-    type: REMOVE_NOTE_FROM_NOTEBOOK,
-    noteId
-  });
-}
+export const deleteNotebook = notebook => dispatch => {
+  dispatch(loadNotebooks());
+  return NotebookAPIUtil.deleteNotebook(notebook.id).then(
+    () => dispatch(removeNotebook(notebook.id)),
+    err => dispatch(receiveNotebookErrors(err.responseJSON))
+  );
+};
 
-
-
-export const updateNotebook = (notebook) => {
-  return (dispatch) => {
-    dispatch(loadNotebooks());
-    return NotebookAPIUtil.updateNotebook(notebook).then((notebook) => {
-      return dispatch(receiveUpdatedNotebook(notebook));
-    },
-      (err) => {
-        return dispatch(receiveNotebookErrors(err.responseJSON));
-      });
-  }
-}
-
-export const createNotebook = (notebook) => {
-  return (dispatch) => {
-    dispatch(loadNotebooks());
-    return NotebookAPIUtil.createNotebook(notebook).then((notebook) => {
-      return dispatch(receiveUpdatedNotebook(notebook));
-    },
-      (err) => {
-        return dispatch(receiveNotebookErrors(err.responseJSON));
-      });
-  }
-}
-
-export const deleteNotebook = (notebook) => {
-  return (dispatch) => {
-    dispatch(loadNotebooks());
-    return NotebookAPIUtil.deleteNotebook(notebook.id).then(() => {
-      return dispatch(removeNotebook(notebook.id));
-    },
-      (err) => {
-        return dispatch(receiveNotebookErrors(err.responseJSON));
-      });
-  }
-}
-
-export const deleteNotebookById = (notebookId) => {
-  return (dispatch) => {
-    dispatch(loadNotebooks());
-    return NotebookAPIUtil.deleteNotebook(notebookId).then(() => {
-      return dispatch(removeNotebook(notebookId));
-    },
-      (err) => {
-        return dispatch(receiveNotebookErrors(err.responseJSON));
-      });
-  }
-}
-
+export const deleteNotebookById = notebookId => dispatch => {
+  dispatch(loadNotebooks());
+  return NotebookAPIUtil.deleteNotebook(notebookId).then(
+    () => dispatch(removeNotebook(notebookId)),
+    err => dispatch(receiveNotebookErrors(err.responseJSON))
+  );
+};
