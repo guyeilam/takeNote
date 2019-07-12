a0dd369e4a54f402f9e02b189ee53d6e55805787
Guy Eilam <guyeilam@gmail.com>
2019-07-12 15:39:56 -0400

    Format with prettier

--- frontend/components/splash/footer.jsx
+++ frontend/components/splash/footer.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => (
-  <div className='footer'>
-    <div className='footer-links'>
-      <div className='footer-linkedin'>
-        <a href="https://www.linkedin.com/in/guyeilam"><i className="fab fa-linkedin"></i></a>
+  <div className="footer">
+    <div className="footer-links">
+      <div className="footer-linkedin">
+        <a href="https://www.linkedin.com/in/guyeilam">
+          <i className="fab fa-linkedin" />
+        </a>
       </div>
-      <div className='footer-github'>
-        <a href="https://github.com/guyeilam"><i className="fab fa-github"></i></a>
+      <div className="footer-github">
+        <a href="https://github.com/guyeilam">
+          <i className="fab fa-github" />
+        </a>
       </div>
-      <div className='footer-angellist'>
-        <a href="https://angel.co/guy-eilam"><i className="fab fa-angellist"></i></a>
+      <div className="footer-angellist">
+        <a href="https://angel.co/guy-eilam">
+          <i className="fab fa-angellist" />
+        </a>
       </div>
     </div>
   </div>
